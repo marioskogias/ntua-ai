f004b12ccedd3f9a208c22b0eb78f13368dd78e7
marioskogias <marioskogias@gmail.com>
2013-03-21 11:39:46 +0200

    Started working on the A*

---
+++ src/Node.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Node implements Comparable<Node>{
+	int initial_move;
+	int cost_so_far;
+	int left_to_reach;
+	int overall_cost;
+	ArrayList<Node> children;
+
+
+	public int compareTo(Node n) {
+		if (n.overall_cost > this.overall_cost)
+			return -1;
+		else if (n.overall_cost < this.overall_cost)
+			return 1;
+		else 
+			return 0;
+	}
+
+
+}
--- src/Robot.java
+++ src/Robot.java
@@ -4,8 +4,38 @@ public class Robot {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	
+	int posX;
+	int posY;
+	int velocity;
+	char [][] place; // to epipedo ergasias
+	
+	public void move(int direction) {
+		switch(direction) {
+			case 1: // 1 de3ia
+				posX = posX+velocity;
+				break;
+			case 2: // 2 aristera
+				posX = posX-velocity;
+				break;
+			case 3: // 3 panw
+				posX = posY+velocity;
+				break;
+			case 4: // 4 katw
+				posX = posY-velocity;
+				break;
+					
 		
+		}
+	}
+	
+	public static void main(String[] args) {
+	
+		Robot r = new Robot();
+		r.posX = 3;
+		r.velocity = 1;
+		r.move(2);
+		System.out.println(r.posX);
 		
 		
 	}

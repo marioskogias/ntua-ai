4509bf5ae67d656b3ba7bdbbec68269b6671dd7e
marioskogias <marioskogias@gmail.com>
2013-03-21 13:39:14 +0200

    check if found in less than 3 moves

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class Main {
 
 	/**
@@ -11,57 +10,67 @@ public class Main {
 	int limX;
 	int limY;
 	char[][] place; // to epipedo ergasias
-	
+
 	public static void main(String[] args) {
-		
+
 		Main m = new Main();
 		Robot me = new Robot();
 		Robot target = new Robot();
-		int i,j;
+		int i, j;
+		// get the input
 		try {
-			BufferedReader in = new BufferedReader (new FileReader ("/Users/marioskogias/Documents/ntua/ai-ntua/src/input.txt")) ;
-			String line = in.readLine () ;
-			String [] a = line.split (" ") ;
-			m.limY = Integer.parseInt(a[0]) ;
-			m.limX = Integer.parseInt(a[1]) ;
+			BufferedReader in = new BufferedReader(new FileReader(
+					"/Users/marioskogias/Documents/ntua/ai-ntua/src/input.txt"));
+			String line = in.readLine();
+			String[] a = line.split(" ");
+			m.limY = Integer.parseInt(a[0]);
+			m.limX = Integer.parseInt(a[1]);
 			me.limX = m.limX;
 			me.limY = m.limY;
 			target.limX = m.limX;
 			target.limY = m.limY;
-			
-			line = in.readLine () ;
-			a = line.split (" ") ;
-			me.posY = Integer.parseInt(a[0]) ;
-			me.posX = Integer.parseInt(a[1]) ;
-			
-			line = in.readLine () ;
-			a = line.split (" ") ;
-			target.posY = Integer.parseInt(a[0]) ;
-			target.posX = Integer.parseInt(a[1]) ;
-			
+
+			line = in.readLine();
+			a = line.split(" ");
+			me.posY = Integer.parseInt(a[0]);
+			me.posX = Integer.parseInt(a[1]);
+			me.velocity = 3;
+			me.name = 'M';
+
+			line = in.readLine();
+			a = line.split(" ");
+			target.posY = Integer.parseInt(a[0]);
+			target.posX = Integer.parseInt(a[1]);
+			target.velocity = 1;
+			target.name = 'T';
+
 			m.place = new char[m.limY][m.limX];
-			for (i=0;i<m.limY;i++){
+			for (i = 0; i < m.limY; i++) {
 				line = in.readLine();
 				char[] array = line.toCharArray();
-				for (j=0;j<m.limX;j++)
+				for (j = 0; j < m.limX; j++)
 					m.place[i][j] = array[j];
 			}
-			
+
 			me.place = m.place;
 			target.place = m.place;
-			
-			in.close();
-		}
-		catch( IOException e ) {
-			e . printStackTrace () ;
-			}
-		
-		for (i=0;i<m.limY;i++){
-			for(j=0;j<m.limX;j++)
-				System.out.print(m.place[i][j]);
-			System.out.println();
 
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-			}
+
+		// start the procedure
+		System.out.println(me.canMove(1));
+		System.out.println(me.canMove(2));
+		System.out.println(me.canMove(3));
+		System.out.println(me.canMove(4));
+		me.move(3);
+		System.out.println("done the move");
+		System.out.println(me.checkFound());
+		
+		
+
+	}
 
 }
--- src/Robot.java
+++ src/Robot.java
@@ -25,12 +25,12 @@ public class Robot {
 			posX = posX - velocity;
 			place[posY][posX] = this.name;
 			break;
-		case 3: // 3 panw
+		case 3: // 3 katw
 			place[posY][posX] = 'O';
 			posX = posY + velocity;
 			place[posY][posX] = this.name;
 			break;
-		case 4: // 4 katw
+		case 4: // 4 panw
 			place[posY][posX] = 'O';
 			posX = posY - velocity;
 			place[posY][posX] = this.name;
@@ -49,21 +49,21 @@ public class Robot {
 			else
 				return 0;
 		case 2: // 2 aristera
-			if ((posX - 3 <= limX) && (this.place[posY][posX - 1] == 'O')
+			if ((posX - 3 >= 0) && (this.place[posY][posX - 1] == 'O')
 					&& (place[posY][posX - 2] == 'O')
 					&& (place[posY][posX - 3] == 'O'))
 				return 1;
 			else
 				return 0;
-		case 3: // 3 panw
-			if ((posY + 3 <= limX) && (place[posY + 1][posX] == 'O')
+		case 3: // 3 katw
+			if ((posY + 3 <= limY) && (place[posY + 1][posX] == 'O')
 					&& (place[posY + 2][posX] == 'O')
 					&& (place[posY + 3][posX] == 'O'))
 				return 1;
 			else
 				return 0;
-		case 4: // 4 katw
-			if ((posY - 3 <= limX) && (place[posY - 1][posX] == 'O')
+		case 4: // 4 panw
+			if ((posY - 3 >=  0) && (place[posY - 1][posX] == 'O')
 					&& (place[posY - 2][posX] == 'O')
 					&& (place[posY - 3][posX] == 'O'))
 				return 1;
@@ -74,7 +74,41 @@ public class Robot {
 		return 0;
 
 	}
-
+	
+	public int checkFound() {
+		int i;
+		for (i=1;i<4 && (posX+i)<limX;i++) { //de3ia
+			if (place[posY][posX + i] == 'X')
+				break;
+			if (place[posY][posX+i] == 'T')
+				return 1;
+		}
+		
+		for (i=1;i<4 && (posX-i)<limX;i++) { //aristera
+			if (place[posY][posX - i] == 'X')
+				break;
+			if (place[posY][posX -i] == 'T')
+				return 1;
+		}
+		
+		for (i=1;i<4 && (posY+i)<limY;i++) { //panw
+			if (place[posY+i][posX] == 'X')
+				break;
+			if (place[posY+i][posX] == 'T')
+				return 1;
+		}
+		
+		for (i=1;i<4 && (posY-i)<limY;i++) { //de3ia
+			if (place[posY-i][posX] == 'X')
+				break;
+			if (place[posY-i][posX] == 'T')
+				return 1;
+		}
+		
+		return 0;
+	}
+	
+	
 	
 
 }

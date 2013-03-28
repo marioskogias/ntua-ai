3c0acfc82a68023cbe7be3120cc76a7dfee95e8b
marioskogias <marioskogias@gmail.com>
2013-03-28 23:34:53 +0200

    Minigitis left

--- exe2/diagnosis.pl
+++ exe2/diagnosis.pl
@@ -6,24 +6,52 @@ start:- write('Welcome to the automated system of nursing. Please answer the fol
 	write('You probably have '),
 	write(X),!.
 
-start:- write('No idea').
+start:- write('The automated system can\'t figure aout what is your problem. You are suggested to visit a human doctor\n').
 
 
 
 % diagnose a Cold
 
 diagnosis(cold) :- symptom(sore_throat),
-		symptom(fever),
 		symptom(cough),
+		sympton(tired),
+		symptom(headache),
 		symptom(short_period_symptoms).
 
+% diagnose a flu
+
+diagnosis(flu) :- symptom(sore_throat),
+			symptom(fever),
+			symptom(nausia_vomit),
+			symptom(feeling_cold),
+			symptom(head_ache),
+			symptom(sneeze),
+			symptom(red_eyes).
 %Symptoms
 
 symptom(fever) :- has(fever),!.
-symptom(fever) :- \+ has_not(fever), write('Do you  have a fever (y/n) ?'),
+symptom(fever) :- \+ has_not(fever), write('Do you have a fever (y/n) ?'),
 			read(Reply),addKnowledge(fever,Reply),
 			Reply='y'.
 
+symptom(sneeze) :- has(sneeze),!.
+symptom(sneeze) :- \+ has_not(sneeze), write('Do you sneeze a lot (y/n) ?'),
+			read(Reply),addKnowledge(sneeze,Reply),
+			Reply='y'.
+
+symptom(red_eyes) :- has(red_eyes),!.
+symptom(red_eyes) :- \+ has_not(red_eyes), write('Are your eyes redish (y/n) ?'),
+			read(Reply),addKnowledge(red_eyes,Reply),
+			Reply='y'.
+
+symptom(feeling_cold) :- has(feeling_cold),!.
+symptom(feeling_cold) :- \+ has_not(feeling_cold), write('Do you often feel cold (y/n) ?'),
+			read(Reply),addKnowledge(feeling_cold,Reply),
+			Reply='y'.
+symptom(headache) :- has(headache),!.
+symptom(headache) :- \+ has_not(headache), write('Do you  have a fever (y/n) ?'),
+			read(Reply),addKnowledge(headache,Reply),
+			Reply='y'.
 
 symptom(sore_throat) :- has(sore_throat),!.
 symptom(sore_throat) :- \+ has_not(sore_throat), write('Do you  have a sore throat (y/n) ?'),
@@ -32,14 +60,24 @@ symptom(sore_throat) :- \+ has_not(sore_throat), write('Do you  have a sore thro
 
 symptom(cough) :- has(cough),!.
 symptom(cough) :- \+ has_not(sore_throat), write('Do you  have cough (y/n) ?'),
-			read(Reply),addKnowledge(fever,Reply),
+			read(Reply),addKnowledge(cough,Reply),
 			Reply='y'.
 
 symptom(short_period_symptoms) :- has(short_period_symptoms),!.
 symptom(short_period_symptoms) :- \+ has_not(short_period_symptoms), write('Do you have these symptoms for less than a week (y/n) ?'),
-				read(Reply),addKnowledge(fever,Reply),
+				read(Reply),addKnowledge(short_period_sympotoms,Reply),
 				Reply='y'.
 
+
+symptom(tired) :- has(tired),!.
+symptom(tired) :- \+ has_not(tired), write('Do you feel tired (y/n) ?'),
+			read(Reply),addKnowledge(tired,Reply),
+			Reply='y'.
+
+symptom(nausia_vomit) :- has(nausia_vomit),!.
+symptom(nausia_vomit) :- \+ has_not(nausia_vomit), write('Do you often feel nausia and vomit (y/n) ?'),
+			read(Reply),addKnowledge(nausia_vomit,Reply),
+			Reply='y'.
 addKnowledge(Cause,'y') :- asserta(has(Cause)).
 addKnowledge(Cause,'n') :- asserta(has_not(Cause)).
 
